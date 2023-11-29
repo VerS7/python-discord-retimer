02bfdd71f68dfef490838cb0cc85ee140fa83a20
Davydov Albert <103847309+VerS7@users.noreply.github.com>
2023-11-29 23:56:36 +0300

    retime descriptions update

--- descriptions.py
+++ descriptions.py
@@ -1,3 +1,14 @@
 """
 File with description strings
 """
+
+
+# ReTime command
+RETIME_DESCRIPTION = "Creates timer with notification."
+RETIME_ARG_NAME = "Name of timer."
+RETIME_ARG_TIME = "%d:%h:%m | Example: 7d:5h:11m / 14h:30m / 1h / 30m / " \
+                  "You can also use something like 30h, 100m, etc..."
+RETIME_ARG_ROLE = "Role to be noticed | Example: @everyone"
+RETIME_ARG_TIMINGS = "Timings before end when notification appears with @ message | " \
+                     "Example: 1h-Hurry up! / 1h:30m-Almost ready!, 30m-Too Close... / " \
+                     "3d:15h-Deadline is approaching, 2d-Hurry!, 12h:30m-Last chance..."
