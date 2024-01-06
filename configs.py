936fa8aa17109c3b5bf9dad60347e64c50c53e0e
Davydov Albert <103847309+VerS7@users.noreply.github.com>
2024-01-06 05:32:15 +0300

    config

--- configs.py
+++ configs.py
@@ -3,4 +3,4 @@ Config file
 """
 TOKEN = "DISCORD BOT TOKEN HERE"
 TIMER_TICK_COOLDOWN = 5  # Cooldown of timer ticks
-GUILD_ID = 697501526272245854  # Optional server id
+GUILD_ID = 0  # Optional server id
